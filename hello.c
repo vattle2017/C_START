ae4bb322000e0f92dc7c665d41af2d3e2fdad889
vattle <vattle@naver.com>
2025-03-21 16:53:37 +0900

    t2

--- hello.c
+++ hello.c
@@ -3,6 +3,6 @@
 int main(void)
 {
     //main에서 올린것
-    printf("hello, GCC\n"); //team 1 here
-    return 0; //team 1 here2,3,4,5
+    printf("hello, GCC\n"); //team 2 here
+    return 0; //team 2 here2,3,4,5
 }
